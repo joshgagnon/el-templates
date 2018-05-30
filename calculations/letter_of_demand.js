7bf378585bb43c4d7a8d3f407ac17417bb8a7c34
Joshua Gagnon <josh.n.gagnon@gmail.com>
2018-05-30 17:35:39 +1000

    added two more templates

---
+++ calculations/letter_of_demand.js
@@ -0,0 +1,11 @@
+module.exports = function calculate(values){
+    var amount = 0;
+    const breakdown = values.breakdown || {};
+    if(breakdown.show === 'Yes'){
+        amount += parseFloat((breakdown.principalSum || {}).amount) || 0;
+        amount += (breakdown.userDefined || []).reduce((acc, d) => {
+            return acc + parseFloat((d || {}).amount) || 0;
+        }, 0);
+    }
+    return {breakdown: {calculatedAmount: amount}};
+}
