43cbb0547d96ff37f23ec3a750bb0e02154f611e
Joshua Gagnon <josh.n.gagnon@gmail.com>
2018-06-14 11:34:27 +1000

    added categories ordering

--- calculations/vendors_settlement_statement.js
+++ calculations/vendors_settlement_statement.js
@@ -4,25 +4,6 @@
 module.exports = function calculate(values, deps){
     var moment= deps.moment;
 
-    function annumFees(settlementDate, data){
-        var days = 0, sumOfRates = 0;
-        if(settlementDate && data){
-            var annumRate = parseFloat(data.annumRate) || 0,
-                startDateRaw = data.startOfYear;
-            if(annumRate && startDateRaw){
-                var settleDate = moment(settlementDate, "D MMMM YYYY");
-                var startDate = moment(startDateRaw, "D MMMM YYYY");
-                if(startDate.isBefore(settleDate)){
-                    days = parseInt(moment.duration(settleDate.diff(startDate)).asDays(), 10);
-                    var daysInYear = moment.duration(startDate.clone().add('1', 'year').diff(startDate)).asDays();
-                    sumOfRates = (annumRate / daysInYear) * days;
-                }
-            }
-        }
-
-        return {days: days, amount: sumOfRates};
-    }
-
     var debits = parseFloat(values.purchaseAmount) || 0;
     var credits = parseFloat(values.depositAmount) || 0;
 
@@ -36,9 +17,9 @@ module.exports = function calculate(values, deps){
         else{
             var start =  moment(item.startDate, "D MMMM YYYY");
             var end =  moment(item.endDate, "D MMMM YYYY");
-            var periodDays = parseInt(moment.duration(end.diff(start)).asDays(), 10);
+            var periodDays = parseInt(moment.duration(end.diff(start)).asDays(), 10) + 1;
 
-            var apportionmentDays = parseInt(moment.duration(settlementDate.diff(start)).asDays(), 10);
+            var apportionmentDays = parseInt(moment.duration(settlementDate.diff(start)).asDays(), 10) + 1;
             var periodAmount = parseFloat(item.periodAmount) || 0;
             var perDay = periodAmount / periodDays;
             var amount = perDay * apportionmentDays;
