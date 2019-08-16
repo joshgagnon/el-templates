9ff65269a4dee8b301c1df0e69ac223836512f7a
Josh Gagnon <josh.n.gagnon@gmail.com>
2019-08-16 11:37:32 +1000

    using currency library

--- calculations/vendors_settlement_statement.js
+++ calculations/vendors_settlement_statement.js
@@ -3,34 +3,34 @@
 
 module.exports = function calculate(values, deps){
     var moment= deps.moment;
-
-    var debits = parseFloat(values.purchaseAmount) || 0;
-    var credits = parseFloat(values.depositAmount) || 0;
+    var currency = deps.currency;
+    var debits = currency(values.purchaseAmount || 0);
+    var credits = currency(values.depositAmount || 0);
 
 
     var ratesAndApportionments = values.ratesAndApportionments;
     var settlementDate = moment(values.settlementDate, "D MMMM YYYY");
     ratesAndApportionments.map(item => {
         if(item.type === 'rate'){
-            debits += parseFloat(item.amount) || 0;
+            debits = debits.add(currency(item.amount || 0));
         }
         else{
             var start =  moment(item.startDate, "D MMMM YYYY");
-            var end =  start.clone().add('year', 1);
+            var end =  start.clone().add(1, 'year');
 
             var periodDays = parseInt(moment.duration(end.diff(start)).asDays(), 10);
             var apportionmentDays = parseInt(moment.duration(settlementDate.diff(start)).asDays(), 10) + 1;
-            var periodAmount = parseFloat(item.periodAmount) || 0;
-            var perDay = periodAmount / periodDays;
-            var amount = perDay * apportionmentDays;
+            var periodAmount = currency(item.periodAmount || 0);
+            var perDay = periodAmount.divide(periodDays)
+            var amount = perDay.multiply(apportionmentDays);
             item.amount = amount;
             item.days = apportionmentDays;
-            credits += amount;
+            credits = credits.add(amount);
         }
     });
 
-    var finalAmount = debits - credits;
-    var total = Math.max(debits, credits);
+    var finalAmount = debits.subtract(credits);
+
 
     return {calculatedRatesAndApportionments: ratesAndApportionments, totalReceipts: debits, totalPayments: credits};
 }
